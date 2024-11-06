40f7b5d4e332647e98a697dfff56157d6e45ef6d
sodychoe <sodychoe@icloud.com>
2024-11-06 22:57:19 +0900

    Persistence Layer 테스트 2

---
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -0,0 +1,63 @@
+package sample.cafekiosk.spring.domain.product;
+
+import static org.assertj.core.api.Assertions.tuple;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    @DisplayName("원하는 판매상태를 가진 상품들을 조회한다.")
+    void findAllBySellingStatusIn() {
+        // given
+        Product p1 = Product.builder()
+                .productNumber("001")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(4000)
+                .build();
+
+        Product p2 = Product.builder()
+                .productNumber("002")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(HOLD)
+                .name("카페라떼")
+                .price(4500)
+                .build();
+
+        Product p3 = Product.builder()
+                .productNumber("003")
+                .type(ProductType.HANDMADE)
+                .sellingStatus(STOP_SELLING)
+                .name("팥빙수")
+                .price(7000)
+                .build();
+
+        productRepository.saveAll((List.of(p1, p2, p3)));
+
+        // when
+        List<Product> products = productRepository.findAllBySellingStatusIn(List.of(SELLING, HOLD));
+
+        // then
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "sellingStatus")
+                .containsExactlyInAnyOrder(
+                        tuple("001", "아메리카노", SELLING),
+                        tuple("002", "카페라떼", HOLD)
+                );
+    }
+
+}
